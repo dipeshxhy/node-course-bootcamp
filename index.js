6776713668bbb2e6b205951fac86df38ff27c006
dipeshxhy <babasarkar260@gmail.comm>
2025-05-21 12:38:02 +0545

    fs module read and write file

--- index.js
+++ index.js
@@ -1,4 +1,25 @@
 const fs = require("fs");
 
-const hello = "Hello world";
-console.log(hello);
+// const hello = "Hello world";
+// console.log(hello);
+
+//read file
+// const textInput = fs.readFileSync("./txt/file.txt", "utf-8");
+// console.log(textInput);
+
+//write file
+// const textOutput = `This is a new text file: ${textInput}`;
+// fs.writeFileSync("./txt/output.txt", textOutput);
+// console.log("File written successfully");
+
+// non-lock
+fs.readFile("./txt/start.txt", "utf-8", (err, data1) => {
+  if (err) return console.log("Error reading file 💥");
+  fs.readFile(`./txt/${data1.trim()}.txt`, "utf-8", (err, data2) => {
+    if (err) return console.log("Error reading file 💥");
+    fs.writeFile(`./txt/final.txt`, data2, (err) => {
+      if (err) return console.log("Error writing file 💥");
+      console.log("File written successfully");
+    });
+  });
+});
