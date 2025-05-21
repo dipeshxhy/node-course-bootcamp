b630737a30f31084c9de1a28e60ae2d5358f905a
dipeshxhy <babasarkar260@gmail.comm>
2025-05-21 16:44:03 +0545

    templating done

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 const http = require("http");
 const path = require("path");
 const url = require("url");
+const fs = require("fs");
+
+const data = fs.readFileSync(`./dev-data/data.json`, "utf-8");
+const dataObj = JSON.parse(data);
 
 const server = http.createServer((req, res) => {
   const pathName = req.url;
@@ -8,6 +12,11 @@ const server = http.createServer((req, res) => {
     res.end("This is the overview");
   } else if (pathName === "/product") {
     res.end("This is the product");
+  } else if (pathName === "/api") {
+    res.writeHead(200, {
+      "Content-type": "application/json",
+    });
+    res.end(data);
   } else {
     res.writeHead(404, {
       "Content-type": "text/html",
