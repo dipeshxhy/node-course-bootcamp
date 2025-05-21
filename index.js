da2651735b6f693d92e04272c80859795513c488
dipeshxhy <babasarkar260@gmail.comm>
2025-05-21 13:16:54 +0545

    server create and basic routing setup

--- index.js
+++ index.js
@@ -1,25 +1,21 @@
-const fs = require("fs");
+const http = require("http");
+const path = require("path");
+const url = require("url");
 
-// const hello = "Hello world";
-// console.log(hello);
-
-//read file
-// const textInput = fs.readFileSync("./txt/file.txt", "utf-8");
-// console.log(textInput);
-
-//write file
-// const textOutput = `This is a new text file: ${textInput}`;
-// fs.writeFileSync("./txt/output.txt", textOutput);
-// console.log("File written successfully");
-
-// non-lock
-fs.readFile("./txt/start.txt", "utf-8", (err, data1) => {
-  if (err) return console.log("Error reading file 💥");
-  fs.readFile(`./txt/${data1.trim()}.txt`, "utf-8", (err, data2) => {
-    if (err) return console.log("Error reading file 💥");
-    fs.writeFile(`./txt/final.txt`, data2, (err) => {
-      if (err) return console.log("Error writing file 💥");
-      console.log("File written successfully");
+const server = http.createServer((req, res) => {
+  const pathName = req.url;
+  if (pathName === "/overview" || pathName === "/") {
+    res.end("This is the overview");
+  } else if (pathName === "/product") {
+    res.end("This is the product");
+  } else {
+    res.writeHead(404, {
+      "Content-type": "text/html",
     });
-  });
+    res.end("<h1>Page not found</h1>");
+  }
+});
+
+server.listen(8000, "127.0.0.1", () => {
+  console.log("Listening to requests on port 8000");
 });
